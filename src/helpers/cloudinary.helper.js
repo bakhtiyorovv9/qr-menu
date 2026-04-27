81210620161d62d5c512342198518cfe8d9d921a
Faxriddin <140601430+bakhtiyorovv9@users.noreply.github.com>
2026-04-27 18:51:27 +0500

    Add dotenv configuration for Cloudinary
    Load environment variables from .env file for Cloudinary configuration.

--- src/helpers/cloudinary.helper.js
+++ src/helpers/cloudinary.helper.js
@@ -1,5 +1,8 @@
 import { v2 as cloudinary } from "cloudinary";
 import { Readable } from "stream";
+import { config } from "dotenv";
+
+config();
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
